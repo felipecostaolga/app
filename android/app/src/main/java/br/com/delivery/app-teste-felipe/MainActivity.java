49af8bba680736b0ed7cb9c09fe575ebe69b4338
Felipe Costa <felipe.costa@mblabs.com.br>
2024-12-24 01:32:35 -0300

    Atualização de app app-teste-felipe

--- android/app/src/main/java/br/com/delivery/app-teste-felipe/MainActivity.java
+++ android/app/src/main/java/br/com/delivery/app-teste-felipe/MainActivity.java
@@ -1,4 +1,4 @@
-package undefined;
+package br.com.delivery.app-teste-felipe;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/br/com/delivery/app-teste-felipe/MainApplication.java
+++ android/app/src/main/java/br/com/delivery/app-teste-felipe/MainApplication.java
@@ -1,4 +1,4 @@
-package undefined;
+package br.com.delivery.app-teste-felipe;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
