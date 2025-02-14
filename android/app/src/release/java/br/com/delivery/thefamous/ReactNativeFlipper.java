e3a2980523d3dd116dfa6ee640b6fa630ba9d111
Felipe Costa <felipe.costa@mblabs.com.br>
2025-02-14 00:45:55 -0300

    fix::update::atualizando saldo

--- android/app/src/release/java/br/com/delivery/thefamous/ReactNativeFlipper.java
+++ android/app/src/release/java/br/com/delivery/thefamous/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package br.com.delivery.mundoanimaldelivery;
+package br.com.delivery.thefamous;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
